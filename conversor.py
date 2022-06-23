652b379c5c6c13c91d4e8fc60b9410546a97582b
Fatima <faty.aome.rojas14@gmail.com>
2022-06-23 08:07:39 -0500

    condicionales

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input ("Cuanto dinero tienes?: ")
+pesos = float(pesos)
+valor_dolar = 20.20 
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+#Print es para imprimir el contenido en la pantalla
+print("Tienes $" + dolares + "dolares")
