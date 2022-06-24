763c4c0014c77a576d7f08b8d17dac8a168725ea
Fatima <faty.aome.rojas14@gmail.com>
2022-06-23 23:27:17 -0500

    menu funciones

--- funciones.py
+++ funciones.py
@@ -1,8 +1,30 @@
-def imprimir_mensaje():
-    print('mensaje especial: ')
-    print('Estoy aprendiendo a usar funciones! ')
+#def imprimir_mensaje():
+    #print('mensaje especial: ')
+    #print('Estoy aprendiendo a usar funciones! ')
 
 
-imprimir_mensaje()
-imprimir_mensaje()
-imprimir_mensaje()
+#imprimir_mensaje()
+#imprimir_mensaje()
+#imprimir_mensaje()
+
+opcion = int(input('Elige una opcion (1, 2, 3): '))
+if opcion ==1:
+    print('Hola')
+    print('Como estas??')
+    print('Elegiste la opcion 1' )
+    print('Adios')
+
+elif opcion ==2:
+    print('Hola')
+    print('Como estas??')
+    print('Elegiste la opcion 2' )
+    print('Adios')
+
+elif opcion ==3:
+    print('Hola')
+    print('Como estas??')
+    print('Elegiste la opcion 3' )
+    print('Adios')
+
+else:
+    print('Por favor ingresa la opcion correcta')
