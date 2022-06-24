db5490384973d254b3e53c2dcd5c916c4ba3304e
Fatima <faty.aome.rojas14@gmail.com>
2022-06-23 23:30:48 -0500

    modulo_funciones

--- funciones.py
+++ funciones.py
@@ -7,24 +7,22 @@
 #imprimir_mensaje()
 #imprimir_mensaje()
 
+def conversacion(mensaje):
+    print('Hola')
+    print('Como estas??')
+    print(mensaje )
+    print('Adios')
+
+
 opcion = int(input('Elige una opcion (1, 2, 3): '))
 if opcion ==1:
-    print('Hola')
-    print('Como estas??')
-    print('Elegiste la opcion 1' )
-    print('Adios')
+    conversacion('Elegiste la opcion 1')
 
 elif opcion ==2:
-    print('Hola')
-    print('Como estas??')
-    print('Elegiste la opcion 2' )
-    print('Adios')
+    conversacion('Elegiste la opcion 2')
 
 elif opcion ==3:
-    print('Hola')
-    print('Como estas??')
-    print('Elegiste la opcion 3' )
-    print('Adios')
+    conversacion('Elegiste la opcion 3')
 
 else:
     print('Por favor ingresa la opcion correcta')
