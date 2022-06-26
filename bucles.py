5ae51b7ab834912d78f82559da6aef99466779b7
Fatima <faty.aome.rojas14@gmail.com>
2022-06-25 21:39:42 -0500

    for-bucles

--- bucles.py
+++ bucles.py
@@ -17,16 +17,19 @@
 #contador = 5
 #print('2 elevado a ' + str(contador) + ' es igual a ' + str(2**contador))
 
-def run():
-    LIMITE = 1000
+#def run():
+    #LIMITE = 1000
 
-    contador = 0
-    potencia_2 = 2**contador
-    while potencia_2 < LIMITE:
-        print('2 elevado a ' + str(contador) +
-              ' es igual a:' + str(potencia_2))
-        contador = contador + 1
-        potencia_2 = 2**contador
+    #contador = 0
+    #potencia_2 = 2**contador
+    #while potencia_2 < LIMITE:
+        #print('2 elevado a ' + str(contador) +
+              #' es igual a:' + str(potencia_2))
+       #contador = contador + 1
+        #potencia_2 = 2**contador
 
-if __name__ == '__main__':
-    run()
+#if __name__ == '__main__':
+    #run()
+    
+for contador in range(1, 1001):
+    print(contador)
