b1a580f6ca9ac7f1e2aec97330c91dec92341acd
Fatima <faty.aome.rojas14@gmail.com>
2022-06-26 01:26:44 -0500

    ejemplo con break

--- breack_continue.py
+++ breack_continue.py
@@ -6,11 +6,18 @@ def run():
         #apartir de lo que este de la palabra continue no se ejecurata 
         #print(contador)
 
-        for i in range(10000):
+        #for i in range(10000):
         #la variable i en el ciclo for es un estandar (es para que se vea mas pro)
-            print(i)
-            if i == 5678:
-                break #break es para cortar el ciclo definitivamente
+            #print(i)
+            #if i == 5678:
+                #break #break es para cortar el ciclo definitivamente
+
+        texto = input('Escribe un texto: ')
+        for letra in texto:
+            if letra == 'o':
+                break
+            print(letra)
+
 
 if __name__ == '__main__':
     run()
