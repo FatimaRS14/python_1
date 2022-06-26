328bbd24c30549493fb7521cc6f681c559ad6049
Fatima <faty.aome.rojas14@gmail.com>
2022-06-26 18:27:29 -0500

    imprimiendo diccionario con cadena de texto

--- diccionarios.py
+++ diccionarios.py
@@ -5,8 +5,32 @@ def run():
     'llave3': 3,
     }
 
-    print(mi_diccionario)
+    #print(mi_diccionario)
+    #print(mi_diccionario['llave1'])
+    #print(mi_diccionario['llave2'])
+    #print(mi_diccionario['llave3'])
 
+    vews_dramas ={
+        'The Untamed': 30000,
+        'f4 Thailand': 4560,
+        'Leyend of Fei': 24567,
+
+
+
+    }
+
+    #print(vews_dramas['f4 Thailand'])
+    #como recorrer el diccionario y que regrese los datos
+    #for dramas in vews_dramas.keys():
+        #print(dramas)
+        #con esta forma de declarasion del metodo se imprimen los valores de las llaves 
+
+    #for dramas in vews_dramas.values():
+        #print(dramas)
+# de este modo se imprimen los valores 
+     #la palabra especial items sirve para 
+    for dramas, vews in vews_dramas.items():
+        print(dramas + ' tiene ' + str(vews) + ' vistas ')
 
 if __name__ == '__main__':
     run()
