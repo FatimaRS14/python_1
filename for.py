bfe74e534df152a9e9c9a7d1fc17a313145ffd86
Fatima <faty.aome.rojas14@gmail.com>
2022-06-25 21:53:34 -0500

    ultima parte for

--- for.py
+++ for.py
@@ -13,5 +13,8 @@
 #a = list(range(1000))
 #print(a)
 
-for contador in range(1, 1001):
-    print(contador)
+#for contador in range(1, 1001):
+    #print(contador)
+
+for i in range(10):
+    print(11 * 1)
